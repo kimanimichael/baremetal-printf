31b7ce0fb038f51f4ebe300070146e7cde983f59
mikael <muchunumike@gmail.com>
2025-07-16 02:14:25 +0300

    feat(uart_for_debugging): Write simple uart test

--- app/src/main.c
+++ app/src/main.c
@@ -1,12 +1,19 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include "../include/bsp.h"
+#include "../../common/include/core/uart.h"
 
 int main(){
 	BSP_init();
+	uart_setup();
 	while (true){
 		BSP_blueLedToggle();
-		BSP_Delay(1000);
+		BSP_Delay(100);
+
+		if(uart_data_available()) {
+		uint8_t data = uart_read_byte();
+		uart_write_byte(data);
+	}
 	}
 }
 
