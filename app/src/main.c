19249228d0887492b5d583ee90f0da71b31c3995
mikael <muchunumike@gmail.com>
2025-07-16 13:39:33 +0300

    feat(uart_for_debugging): Add logging with string literals support only

--- app/src/main.c
+++ app/src/main.c
@@ -3,14 +3,28 @@
 #include "../include/bsp.h"
 #include "../../common/include/core/uart.h"
 
+static uint8_t tiny_strlen(const char *s) {
+    uint8_t len = 0;
+    while (*s++ && (len < UINT8_MAX)) {
+        len++;
+    }
+    return len;
+}
+
+static void mk_logging(const char * log) {
+	uart_write((uint8_t *)log, tiny_strlen(log));
+	uart_write((uint8_t *)"\r\n", 2);  
+}
+
 int main(){
 	BSP_init();
 	uart_setup();
 
 	uint32_t start = BSP_Tickr();
 	while (true){
-		if ((BSP_Tickr() - start) > 100) {
+		if ((BSP_Tickr() - start) > 1000) {
 			BSP_blueLedToggle();
+			mk_logging("LED toggled");
 			start = BSP_Tickr();
 		}
 
