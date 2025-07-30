10375d26a26443b54fa3563fbe4e3eeb37da7495
mikael <muchunumike@gmail.com>
2025-07-30 09:49:59 +0300

    feat(build_with_python_and_cmake): Add python script for building

--- build_flash.sh
+++ build_flash.sh
@@ -1,5 +1,6 @@
 cmake -S ./bootloader/  -B bootloader/cmake-build/ -G Ninja
 cmake --build bootloader/cmake-build/
+cp bootloader/cmake-build/baremetal_bootloader.bin dev_tools/
 
 cmake -S .  -B cmake-build/ -G Ninja
 cmake --build cmake-build/
